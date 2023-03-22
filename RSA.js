b5ec8b90e9f21f77e304699d28bc8c9221d8dd1c
avinash <singhavinash6641@gmail.com>
2023-03-22 09:44:15 +0530

    Aes simulation implemented

--- RSA.js
+++ RSA.js
@@ -29,6 +29,33 @@ function rsaPublicKey_D_Generator(E){
        }
      }
 }
+function fastModulerExponent(m,E,Mod){
+  var binaryOfExponent = parseInt(E,16).toString(2);
+  console.log(binaryOfExponent);
+  let result = m;
+  for(let i = 1;i<binaryOfExponent.length;i++){
+    if(binaryOfExponent.charAt(i)==0){
+       result = (result^2)%Mod;
+    }
+    else if(binaryOfExponent.charAt(i)==1){
+      result = (result^2)%Mod;
+      result = (result*m)%Mod;
+    }
+  }
+  return result;
+}
+function powerMod(base, exponent, modulus) {
+  if (modulus === 1) return 0;
+  var result = 1;
+  base = base % modulus;
+  while (exponent > 0) {
+      if (exponent % 2 === 1)  //odd number
+          result = (result * base) % modulus;
+      exponent = exponent >> 1; //divide by 2
+      base = (base * base) % modulus;
+  }
+  return result;
+}
 function rsaPQNL(){
     setRsaDocument();
     console.log(rsaPrimeP.value);
@@ -48,4 +75,7 @@ function rsaPQNL(){
     console.log(possibleEValueArray);
     console.log(rsaPublicKey_D_Generator(38539));
     console.log(possibleDValueArray);
+    console.log("here",powerMod(101,38537,39203));
+    console.log("here",powerMod(31975,87497,39203));
+    
 }
